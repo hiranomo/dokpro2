4bb1d3418d483bbce4d0257cff14be52721bbecb
hiranomo <hirano-1997@i.softbank.jp>
2019-10-31 00:34:24 +0900

    pycharm first commit

---
+++ hangman.py
@@ -0,0 +1,16 @@
+print('hangman')
+i: int
+for i in range(9):
+    i += i
+    print(i)
+
+
+def mmm(v: int):
+    print(v)
+
+
+member = {"name": "Tim", "age": 32, "sex": "men"}
+
+propertys = []
+
+
